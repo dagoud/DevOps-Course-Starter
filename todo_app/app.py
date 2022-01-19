83483e0dd75b3acd5a5ab24a581475d04652c28a
lv31402 <david.goudie@lv.co.uk>
2022-01-19 23:26:43 +0000

    Revert "DG No Issue: todo app working and adding items"
    This reverts commit d0ed4c4e70436f8b6bc25677e173c5058b969d23.

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,7 +1,4 @@
 from flask import Flask
-from flask import render_template
-from flask import request
-from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -11,17 +8,4 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    items = get_items()
-    
-    return render_template('index.html', items = items)
-
-
-@app.route('/item', methods=['POST']) 
-def add_item_to_list():
-
-    itemTitle = request.form.get('item-input')
-    addItem = add_item(itemTitle)
-    print(addItem)
-    
-    # return 
-    return addItem
+    return 'Hello World!'
