d0ed4c4e70436f8b6bc25677e173c5058b969d23
lv31402 <david.goudie@lv.co.uk>
2022-01-19 14:30:17 +0000

    DG No Issue: todo app working and adding items

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,7 @@
 from flask import Flask
+from flask import render_template
+from flask import request
+from todo_app.data.session_items import add_item, get_items
 
 from todo_app.flask_config import Config
 
@@ -8,4 +11,17 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    items = get_items()
+    
+    return render_template('index.html', items = items)
+
+
+@app.route('/item', methods=['POST']) 
+def add_item_to_list():
+
+    itemTitle = request.form.get('item-input')
+    addItem = add_item(itemTitle)
+    print(addItem)
+    
+    # return 
+    return addItem
